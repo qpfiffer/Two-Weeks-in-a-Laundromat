890e7be7bd68197e93eddb594bbcb8c27c67bcbc
Quinlan Pfiffer <qpfiffer@gmail.com>
2012-02-02 22:17:04 -0800

    Oops.

--- Two Weeks in a Laundromat/Two Weeks in a Laundromat/Two Weeks in a Laundromat/World/Door.cs
+++ Two Weeks in a Laundromat/Two Weeks in a Laundromat/Two Weeks in a Laundromat/World/Door.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Delve_Engine.Utilities;
+using Delve_Engine.DataTypes;
 using Delve_Engine.World;
 
-namespace Two_Weeks_in_a_Laundromat.World
+namespace Two_Weeks_in_a_Laundromat
 {
     class Door: GameObject
     {
+        public Door(ref Vector3 position, ref Vector2 rotation, GraphicsDevice gDevice)
+            : base(ref position, ref rotation, gDevice)
+        {
+        }
     }
 }
--- Two Weeks in a Laundromat/Two Weeks in a Laundromat/Two Weeks in a Laundromat/World/Room.cs
+++ Two Weeks in a Laundromat/Two Weeks in a Laundromat/Two Weeks in a Laundromat/World/Room.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Delve_Engine.Utilities;
+using Delve_Engine.DataTypes;
+using Delve_Engine.World;
 
-namespace Two_Weeks_in_a_Laundromat.World
+namespace Two_Weeks_in_a_Laundromat
 {
     class Room
     {
