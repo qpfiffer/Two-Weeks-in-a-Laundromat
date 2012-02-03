a926c01334e8f660e38ca248e86de69183bb6093
Quinlan Pfiffer <qpfiffer@gmail.com>
2012-02-02 22:06:00 -0800

    Initial commit of room hallway and door.

---
+++ Two Weeks in a Laundromat/Two Weeks in a Laundromat/Two Weeks in a Laundromat/World/Door.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Delve_Engine.World;
+
+namespace Two_Weeks_in_a_Laundromat.World
+{
+    class Door: GameObject
+    {
+    }
+}
---
+++ Two Weeks in a Laundromat/Two Weeks in a Laundromat/Two Weeks in a Laundromat/World/Room.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Two_Weeks_in_a_Laundromat.World
+{
+    class Room
+    {
+    }
+}
